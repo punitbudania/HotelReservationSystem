b3f43175c8108bc256433c005004e1bbb3543240
punitbudania <punitbudania88@gmail.com>
2020-12-05 09:22:14 +0530

    [punit]Added: Welcome Message

---
+++ main/java/com/hotelmanagement/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+package com.hotelmanagement;
+
+public class HotelReservationSystem
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcime to Hotel Reservation System");
+    }
+}
